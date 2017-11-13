ed0f29360361c7b6bd7a0ff41d3161b83218ceca
gabrielibanez <g.ibanezw@gmail.com>
2017-11-13 16:54:52 +0100

    finished home and team page

--- config.rb
+++ config.rb
@@ -21,3 +21,6 @@ activate :deploy do |deploy|
   deploy.deploy_method = :git
 end
 
+["papillard", "ssaunier", "monsieurpaillard"].each do |name|
+  proxy "/flats/#{name}.html", "/flats/show.html", locals: { owner: name }, ignore: true
+end
